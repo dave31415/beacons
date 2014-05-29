09bb3adf0c45f7e9d26ff2eb51f95868e596f073
David <davej@dell.(none)>
2014-05-29 11:11:40 -0400

    added lots of data

--- beacons.py
+++ beacons.py
@@ -1,9 +1,24 @@
 import requests, json
 import urllib, urllib2, cookielib, socket
+import csv, glob
+from collections import defaultdict
+from matplotlib import pylab as plt
+import numpy as np
 
 base_url="http://api.kontakt.io/"
 api_key=open('api.key','r').read().strip()
+root_dir='/home/davej/beacons'
+
+data_dir=root_dir+'/data/dresser'
+plot_dir=root_dir+'/plots'
+
 unique_ids=['dFTG','DuOP','cYOy','aqO9','HMGV']
+names=['Bathroom','Bedroom','Boys room','Gwens Room','Kitchen']
+macs=['E8:66:CA:46:29:D9','C0:80:69:E4:96:80','C0:36:4C:60:FB:7A','C4:02:3E:D6:FB:17','CB:42:69:CE:8C:14']
+mac_lookup={}
+for i in range(5):
+    mac_lookup[macs[i]]={'mac':macs[i],'uid':unique_ids[i],'name':names[i]}
+
 
 def get_beacons(num=0):
     url=base_url+'beacon/'+unique_ids[num] 
@@ -59,8 +74,58 @@ def post2_beacons(num=0):
     res = opener.open(req)
     return res
     
+def read_csv_dump(filename='2014-0529-07-02-16.csv'):
+    file_name=data_dir+'/'+filename
+    data=list(csv.DictReader(open(file_name,'rU')))
+    #filter out non-ibeacons for now
+    data=[d for d in data if d['iBeacon flag'] == 'true'] 
+    for d in data:
+        mac=d['MAC Addr']
+        d.update(mac_lookup[mac])
+    return data
+
+def read_all():
+    files=glob.glob(data_dir+'/*')        
+    dat=defaultdict(list)
+
+    for f in files:
+        filename=f.split('/')[-1]
+        data=read_csv_dump(filename)
+        for line in data:    
+            name=line['name']
+            dat[name].append(line)
+    return dat
     
-    
-    
-    
-    
+def plot_all(data):
+    legs=[]
+    mean_rssi=[]
+    mean_rssi_err=[]
+    max_x=0
+    xextra=10
+    fig=plt.figure()
+    for name,dat in data.iteritems():
+        num=len(dat)
+        max_x=max(max_x,num)
+        print name
+        legs.append(name)
+        ts=[i['Last Updated'] for i in dat]
+        rssi=np.array([float(i['RSSI']) for i in dat])
+        print ts
+        print rssi
+        mean_rssi.append(rssi.mean())
+        mean_rssi_err.append(rssi.std()/np.sqrt(num))
+        x=np.arange(num)
+        plt.plot(x,rssi,linewidth=3)
+    for m,merr in zip(mean_rssi,mean_rssi_err):
+        plt.hlines(m,0,num+10,linestyle='--')
+        plt.hlines(m-merr,0,num+xextra,linestyle='dotted')
+        plt.hlines(m+merr,0,num+xextra,linestyle='dotted')        
+        plt.fill_between([0,num+xextra],m+merr,m-merr,alpha=0.2)
+    plt.xlim=(0,max_x+20)
+    plt.ylabel('Signal Strength  ( RSSI )')
+    plt.xlabel('Iteration')
+    plt.legend(legs)
+    fig.savefig(plot_dir+'/dresser_rssi.png')
+    plt.show()    
+
+
