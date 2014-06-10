7397b90f1fdcee865317b2d98c4f46afa31da1e4
David Johnston <davej@dajohnst.local>
2014-06-10 10:59:54 -0400

    adding moved files

---
+++ logger.py
@@ -0,0 +1,146 @@
+# all the imports
+from __future__ import with_statement
+import sqlite3
+from contextlib import closing
+from flask import Flask, request, g, url_for, render_template
+from lowess_tmp import lowess
+import numpy as np
+import smoothing
+from util import *
+
+from PARAMS import *
+
+#important parameters and defaults
+
+DEBUG = True
+SHOW_MAX=100
+F_LOWESS=0.1
+STATS_WINDOW_SEC=15
+ERR_MIN=0.05
+REFRESH_RATE_SEC=5000000000
+SMOOTHING_TYPE='windowed'
+BOX_SM_SEC=20.0
+SS_ZPT=100
+TIME_SUBTRACT=698000+14500
+NO_SIGNAL_VALUE=0.3
+JITTER=0.3
+SHOW_ALL="yes"
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+JSON_DC=json.JSONDecoder()
+JSON_EC=json.JSONEncoder()
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+@app.route('/',methods=['POST','GET'])
+def chart_latest():
+    #form changable parameters
+    message=request.form.get('message', '') 
+    showmax=int(request.form.get('showmax', SHOW_MAX))
+    smoothing_type=request.form.get('smoothing_type', SMOOTHING_TYPE)
+    f_lowess=float(request.form.get('f_lowess', F_LOWESS))
+    box_sm_sec=float(request.form.get('box_sm_sec', BOX_SM_SEC))
+    refresh_rate_sec=int(request.form.get('refresh_rate_sec', REFRESH_RATE_SEC))
+    showall=request.form.get('showall', SHOW_ALL)
+
+    #TODO: careful about sorting by date string. Not correct!!  Won't handle Month boundaries right
+    cur = g.db.execute('select * from entries order by date_str DESC limit %s'%showmax)
+    entries = [add_mac(dict(id=row[0], uuid=row[1], major=row[2], minor=row[3], rssi=row[4], date_str=row[5])) for row in cur.fetchall()]
+    #entries is a list of dicts, get unique MACs
+    macs=list({e['MAC'] for e in entries})
+    macs=sorted(macs)
+    n_macs=len(macs)
+    xs_dict={}
+    columns_list=[]
+    x_all=[]
+    data_all=[]
+    for i in xrange(n_macs) :
+        ii=str(i)
+        data_name="data"+ii
+        x_name='x'+ii
+        x=[e['epoch']-TIME_SUBTRACT+JITTER*np.random.randn() for e in entries if e['MAC']==macs[i]]
+        dat=[int(e['ss']) for e in entries if e['MAC']==macs[i]]
+        x_all=x_all+x
+        data_all=data_all+dat
+        
+        if  showall == 'yes':
+            xs_dict[data_name]=x_name
+            columns_list.append([x_name]+x)
+            columns_list.append([data_name]+dat)
+        
+    x_all=np.array(x_all)
+    data_all=np.array(data_all)
+
+    so=np.argsort(x_all)
+    x_all=x_all[so]
+    data_all=data_all[so]
+
+    t_zeros=[]
+
+    if smoothing_type == 'lowess' :
+        data_smooth=lowess(x_all,data_all,f=f_lowess,iter=3)
+    if smoothing_type == 'exponential' :
+        data_smooth=smoothing.smooth_exp(data_all,scale=exp_sm_scale)
+    if smoothing_type == 'windowed' :
+        data_smooth,t_zeros = smoothing.smooth(x_all,data_all,B=box_sm_sec,beta=1.0)
+        
+    no_signal=[NO_SIGNAL_VALUE for i in t_zeros] 
+    
+    xs_dict['data_all']='x_all'
+    columns_list.append(['x_all']+t_zeros+list(x_all))
+    columns_list.append(['data_all']+no_signal+list(data_smooth))
+    #a hack just for the colors
+    columns_list=list(reversed(columns_list))
+    
+    time_last=x_all[-1]
+    window=x_all > (time_last-STATS_WINDOW_SEC)
+    
+    ss_mean="%0.1f" % data_all[window].mean()
+    ss_sigma="%0.2f" % np.sqrt(data_all[window].std()**2 + ERR_MIN**2)
+    ss_mean_smooth="%0.1f" % data_smooth[window].mean()
+    ss_sigma_smooth="%0.2f" % np.sqrt(data_smooth[window].std()**2+ERR_MIN**2)
+
+    dist_m_numeric=smoothing.dist_power_law(float(ss_mean_smooth)-SS_ZPT)
+    dist_m="%0.2f" % dist_m_numeric
+
+    return render_template('log_charts.html', entries=entries,xs=xs_dict,columns=columns_list,ss_mean=ss_mean,ss_sigma=ss_sigma,
+                           ss_mean_smooth=ss_mean_smooth,ss_sigma_smooth=ss_sigma_smooth,message=message, dist_m=dist_m,
+                           showmax=showmax,smoothing_type=smoothing_type,f_lowess=f_lowess,box_sm_sec=box_sm_sec,
+                           showall=showall,
+                           refresh_rate_sec=refresh_rate_sec)
+
+@app.route('/all')
+def show_all():
+    cur = g.db.execute('select * from entries order by date_str DESC')
+    entries = [add_mac(dict(id=row[0], uuid=row[1], major=row[2], minor=row[3], rssi=row[4], date_str=row[5])) for row in cur.fetchall()]
+    return render_template('log_all.html', entries=entries)
+
+@app.route('/submit', methods=['POST'])
+def submit_entry():
+    print "submission received"
+    g.db.execute('insert into entries (uuid, major,minor,rssi,date_str) values (?, ?, ?, ?, ?)',
+            [request.form['uuid'], request.form['major'], request.form['minor'], request.form['rssi'], request.form['date_str']])
+    g.db.commit()
+    print "submission entered'"
+    return 'this is ok'
+
+if __name__ == '__main__':
+    app.run(port=PORT)
---
+++ smoothing.py
@@ -0,0 +1,144 @@
+import numpy as np
+from matplotlib import pylab as plt
+from matplotlib.cbook import flatten
+
+beta_def=17.5
+
+def smooth_exp(y,scale=10.0,N=4):
+    #smooth with expoential tail
+    yrev=np.array(y)[::-1]
+    size=np.ceil(scale*N)
+    weight=np.exp(-np.arange(size)/scale)
+    yrev_sm=yrev*0.0
+    for i in xrange(len(y)):
+        y_window=yrev[i:i+scale*N]
+        #normalize, might be truncated
+        w=weight[0:len(y_window)]
+        w=w/w.sum()
+        yrev_sm[i]=(y_window*w).sum()
+    y_sm=yrev_sm[::-1]
+    return y_sm
+
+def smooth_exp_xy(x,y,scale=10.0,N=4):
+    #smooth with expoential tail backwards in time
+    #should be sorted by x
+    xrev=np.array(x)[::-1]
+    yrev=np.array(y)[::-1]
+    size=np.ceil(scale*N)
+    weight=np.exp(-np.arange(size)/scale)
+    yrev_sm=yrev*0.0
+    for i,xi in enumerate(xrev):
+        x_window=xrev[i:i+scale*N]
+        y_window=yrev[i:i+scale*N]
+        #normalize, could be truncated
+        w=weight[0:len(x_window)]
+        w=w/w.sum()
+        yrev_sm[i]=(y_window*w).sum()
+    y_sm=yrev_sm[::-1]
+    return y_sm
+
+def RSSI_power_law(dist_m,beta=beta_def):
+    RSSI=-77.0-beta*np.log10(dist_m)
+    return RSSI
+
+def dist_power_law(RSSI,beta=beta_def):
+    dist_m = 10.0**((RSSI+77)/(-beta))
+    return dist_m
+
+def prob_dist(RSSI,dist_m,beta=beta_def,sig_RSSI=5.0,number=1.0):
+    #lognormal
+    if RSSI < -100 : return RSSI*0.0
+    R_sig=sig_RSSI/np.sqrt(number)
+    RSSI_mean=RSSI_power_law(dist_m,beta=beta)
+    gauss=np.exp(-0.5*((RSSI-RSSI_mean)/R_sig)**2)
+    return gauss
+
+def grocery_store_im():
+    import matplotlib.image as mpimg
+    file="/Users/davej/Desktop/GrocStore.png"
+    img=mpimg.imread(file)
+    return img
+
+def prob_grid():
+    im_store=grocery_store_im()
+    im=im_store.copy()
+    shape=im.shape
+    plt.imshow(im)
+    print shape
+    nx=shape[0]
+    ny=shape[1]
+    print nx*ny
+    x=np.array([np.arange(nx) for i in xrange(ny)]).flatten()
+    y=np.array([np.repeat(i,nx) for i in xrange(ny)]).flatten()
+
+    imflat=[]
+    for plane in range(3):
+        f=(im[0:,0:,plane]).flatten()
+        print f.shape
+        imflat.append(f)
+
+    xc=nx/2.0
+    yc=ny/2.0
+    rad=100.0
+    mask=np.sqrt((x-xc)**2 + (y-yc)**2 ) < rad
+    #return mask, x,y,imflat
+
+    imflat_g=imflat[0]
+    imflat_g[mask]=0.5
+    imflat_g.shape=(nx,ny)
+
+    im[0:,0:,0]=imflat_g
+    plt.imshow(im)
+
+def filter_window_PL(x,B,beta=0.0,plot=False):    
+    '''filter for smoothing, B is the Box size. 
+    beta=0.0 : box-car smoothing
+    beta=1.0 : straight line from 1 to 0
+    beta=4.5 : approximate exponential smoothing with scale ~ B/5
+    '''
+    rat=(x/float(B))
+    filter=(1.0-rat)**beta
+    filter[(x>B) | (x<0)]=0.0
+    if plot : plt.plot(x,filter)
+    return filter
+
+def windowed_mean(t,y,B,t_point=None,beta=0.3,norm=True,sym=True,flip=False):
+    if t_point == None:
+        t_point=t.max()
+    x=t_point-t    
+    if sym : x=abs(x)
+    if flip: x=-x
+    weight=filter_window_PL(x,B,beta=beta)
+    if norm : weight /= weight.sum()
+    return (weight*y).sum()
+
+def smooth(t,y,B=10.0,beta=1.0,plot=False):
+    sm=y*0.0
+    num=y*0.0
+    epsilon=0.01
+    assert(B>0)
+    t_zeros=[]
+    for i,t_point in enumerate(t):
+        sm[i]=windowed_mean(t,y,B,t_point=t_point,beta=beta)
+        num[i]=windowed_mean(t,y*0+1.0,B,t_point=t_point,beta=0.0,norm=False)
+        num_past=windowed_mean(t,y*0+1.0,B,t_point=t_point,beta=0.0,norm=False,sym=False)
+        num_future=windowed_mean(t,y*0+1.0,B,t_point=t_point,beta=0.0,norm=False,sym=False,flip=True)
+        num_min=min(num_future,num_past)
+        if num_past == 1 :
+            t_zeros.append(t_point-epsilon)
+        if num_future == 1: 
+            t_zeros.append(t_point+epsilon)
+           
+    if plot:
+        plt.plot(t,y)
+        plt.plot(t,sm)
+    
+    return sm,t_zeros
+    
+
+
+
+
+
+
+
